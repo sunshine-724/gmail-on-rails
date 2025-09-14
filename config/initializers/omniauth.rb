9b38053d800fea3cd4123933f4b6eb101dadff0e
中川一樹 <137146752+sunshine-724@users.noreply.github.com>
2025-09-15 02:34:28 +0900

    再度修正

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,7 +1,10 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
+  google_client_id = Rails.application.credentials.dig(:google, :client_id) || ENV["GOOGLE_CLIENT_ID"] || "dummy_client_id"
+  google_client_secret = Rails.application.credentials.dig(:google, :client_secret) || ENV["GOOGLE_CLIENT_SECRET"] || "dummy_client_secret"
+
   provider :google_oauth2,
-           google_client_id: ENV["GOOGLE_CLIENT_ID"] || "dummy_client_id",
-           google_client_secret: ENV["GOOGLE_CLIENT_SECRET"] || "dummy_client_secret",
+           google_client_id,
+           google_client_secret,
            {
              scope: 'userinfo.email, userinfo.profile, gmail.readonly, gmail.send',
              access_type: 'offline',
